5f87a8c8c2654c2a0bb89228a01278af123ed1fa
Robin Wieruch <wrobin@gmx.net>
2017-06-27 19:52:38 +0200

    chapter 1

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
+
+if (module.hot) {
+  module.hot.accept()
+}
