1ee4c812f685c752a7e333a6b886875317800883
Robin Wieruch <wrobin@gmx.net>
2019-03-09 14:06:05 +0530

    Revert "Component Interface with PropTypes (End 4 Chapter)"
    This reverts commit 6ecaaef0bf1f2d5fb6b479037cb10971f35fefe6.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import PropTypes from 'prop-types';
 import './App.css';
 
 const DEFAULT_QUERY = 'redux';
@@ -205,22 +204,10 @@ const Table = ({ list, onDismiss }) =>
     )}
   </div>
 
-Table.propTypes = {
-  list: PropTypes.arrayOf(
-    PropTypes.shape({
-      objectID: PropTypes.string.isRequired,
-      author: PropTypes.string,
-      url: PropTypes.string,
-      num_comments: PropTypes.number,
-      points: PropTypes.number,
-    })
-  ).isRequired,
-  onDismiss: PropTypes.func.isRequired,
-};
 
 const Button = ({
   onClick,
-  className,
+  className = '',
   children,
 }) =>
   <button
@@ -230,16 +217,6 @@ const Button = ({
   >
     {children}
   </button>
-
-Button.defaultProps = {
-  className: '',
-};
-
-Button.propTypes = {
-  onClick: PropTypes.func.isRequired,
-  className: PropTypes.string,
-  children: PropTypes.node.isRequired,
-};
 
 export default App;
 
