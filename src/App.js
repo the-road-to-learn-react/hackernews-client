7304f806e68f30208a827b9aa7ffa5bf5a03a509
Robin Wieruch <wrobin@gmx.net>
2019-03-09 12:03:15 +0530

    Local Component State

--- src/App.js
+++ src/App.js
@@ -21,10 +21,18 @@ const list = [
 ];
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list: list,
+    };
+  }
+
   render() {
     return (
       <div className="App">
-        {list.map(item =>
+        {this.state.list.map(item =>
           <div key={item.objectID}>
             <span>
               <a href={item.url}>{item.title}</a>
