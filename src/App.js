dc46f5efde0059d88a12a52886dd77b85997f78d
Robin Wieruch <wrobin@gmx.net>
2019-03-09 12:01:21 +0530

    ES6 Arrow Functions (Chapter 1 End)

--- src/App.js
+++ src/App.js
@@ -24,18 +24,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {list.map(function(item) {
-          return (
-            <div key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-            </div>
-          );
-        })}
+        {list.map(item =>
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </div>
+        )}
       </div>
     );
   }
