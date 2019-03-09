c214966f5ad6cec05cd4d027f8949c01fe0d5eac
Robin Wieruch <wrobin@gmx.net>
2019-03-09 11:58:13 +0530

    Hot Module Replacement

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+if (module.hot) {
+  module.hot.accept();
+}
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
