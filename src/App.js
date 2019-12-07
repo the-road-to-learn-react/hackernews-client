9963d6c1d0407760906c305bc984e07c256dbea9
Robin Wieruch <wrobin@gmx.net>
2019-12-07 22:45:29 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,46 @@
 import React from 'react';
 
-function getTitle(title) {
-  return title;
-}
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
 function App() {
   return (
     <div>
-      <h1>Hello {getTitle('React')}</h1>
+      <h1>My Hacker Stories</h1>
 
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
+
+      <hr />
+
+      {list.map(function(item) {
+        return (
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </div>
+        );
+      })}
     </div>
   );
 }
