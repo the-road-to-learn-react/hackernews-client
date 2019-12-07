37fbc5bf7c9378413ffea1a98e958f7e8dcf0083
Robin Wieruch <wrobin@gmx.net>
2019-12-07 20:18:05 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
-const title = 'React';
+function getTitle(title) {
+  return title;
+}
 
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Hello {getTitle('React')}</h1>
+
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
     </div>
   );
 }
