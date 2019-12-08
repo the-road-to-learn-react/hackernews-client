3bd47fb99168e42157a8ca704a4d6291980319fb
Robin Wieruch <wrobin@gmx.net>
2019-12-08 15:43:28 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -19,34 +19,29 @@ const list = [
   },
 ];
 
-function App() {
-  return (
-    <div>
-      <h1>My Hacker Stories</h1>
+const App = () => (
+  <div>
+    <h1>My Hacker Stories</h1>
 
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+    <label htmlFor="search">Search: </label>
+    <input id="search" type="text" />
 
-      <hr />
+    <hr />
 
-      <List />
+    <List />
+  </div>
+);
+
+const List = () =>
+  list.map(item => (
+    <div key={item.objectID}>
+      <span>
+        <a href={item.url}>{item.title}</a>
+      </span>
+      <span>{item.author}</span>
+      <span>{item.num_comments}</span>
+      <span>{item.points}</span>
     </div>
-  );
-}
-
-function List() {
-  return list.map(function(item) {
-    return (
-      <div key={item.objectID}>
-        <span>
-          <a href={item.url}>{item.title}</a>
-        </span>
-        <span>{item.author}</span>
-        <span>{item.num_comments}</span>
-        <span>{item.points}</span>
-      </div>
-    );
-  });
-}
+  ));
 
 export default App;
