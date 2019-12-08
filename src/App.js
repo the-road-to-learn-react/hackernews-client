515090c7d5a1eaac976771bd7f20c5ae6f474818
Robin Wieruch <wrobin@gmx.net>
2019-12-08 12:04:55 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -29,20 +29,24 @@ function App() {
 
       <hr />
 
-      {list.map(function(item) {
-        return (
-          <div key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-          </div>
-        );
-      })}
+      <List />
     </div>
   );
+}
+
+function List() {
+  return list.map(function(item) {
+    return (
+      <div key={item.objectID}>
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+      </div>
+    );
+  });
 }
 
 export default App;
