2aea974b23785d8f125781aed9d701f9b9b0b4da
Robin Wieruch <wrobin@gmx.net>
2019-12-09 22:04:57 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -47,29 +47,31 @@ const App = () => {
   );
 };
 
-const Search = props => (
+const Search = ({ value, onSearch }) => (
   <div>
     <label htmlFor="search">Search: </label>
 
     <input
       id="search"
       type="text"
-      value={props.value}
-      onChange={props.onSearch}
+      value={value}
+      onChange={onSearch}
     />
   </div>
 );
 
-const List = props =>
-  props.list.map(item => (
-    <div key={item.objectID}>
-      <span>
-        <a href={item.url}>{item.title}</a>
-      </span>
-      <span>{item.author}</span>
-      <span>{item.num_comments}</span>
-      <span>{item.points}</span>
-    </div>
-  ));
+const List = ({ list }) =>
+  list.map(item => <Item key={item.objectID} item={item} />);
+
+const Item = ({ item }) => (
+  <div>
+    <span>
+      <a href={item.url}>{item.title}</a>
+    </span>
+    <span>{item.author}</span>
+    <span>{item.num_comments}</span>
+    <span>{item.points}</span>
+  </div>
+);
 
 export default App;
