f0a46629ef975f1d965ded14352fc8cd16386c4b
Robin Wieruch <wrobin@gmx.net>
2019-12-09 10:53:01 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
 const App = () => {
-  const handleChange = event => {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
+  // A
+  const handleSearch = event => {
+    // C
     console.log(event.target.value);
   };
 
@@ -28,18 +30,26 @@ const App = () => {
     <div>
       <h1>My Hacker Stories</h1>
 
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" onChange={handleChange} />
+      <Search onSearch={handleSearch} />
 
       <hr />
 
-      <List />
+      <List list={stories} />
     </div>
   );
 };
 
-const List = () =>
-  list.map(item => (
+const Search = props => (
+  <div>
+    <label htmlFor="search">Search: </label>
+
+    {/* B */}
+    <input id="search" type="text" onChange={props.onSearch} />
+  </div>
+);
+
+const List = props =>
+  props.list.map(item => (
     <div key={item.objectID}>
       <span>
         <a href={item.url}>{item.title}</a>
