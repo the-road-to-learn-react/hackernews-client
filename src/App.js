7c58a33fb12b6674bf048f3ea9049fb7a373b10e
Robin Wieruch <wrobin@gmx.net>
2019-12-14 12:44:09 +0800

    c

--- src/App.js
+++ src/App.js
@@ -57,7 +57,7 @@ const App = () => {
     { data: [], isLoading: false, isError: false }
   );
 
-  React.useEffect(() => {
+  const handleFetchStories = React.useCallback(() => {
     if (!searchTerm) return;
 
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
@@ -74,6 +74,10 @@ const App = () => {
         dispatchStories({ type: 'STORIES_FETCH_FAILURE' })
       );
   }, [searchTerm]);
+
+  React.useEffect(() => {
+    handleFetchStories();
+  }, [handleFetchStories]);
 
   const handleRemoveStory = item => {
     dispatchStories({
